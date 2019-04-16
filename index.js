2f824f8963de2b3d5b1e548cb664fdd2b1a15344
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2019-04-16 17:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ function sayHiToGrandma (string) {
   if (string === "hello") {
     return "I can\'t hear you!"}
 
-  elseif (string === "HELLO"){
-    return "YES INDEED!"};
+  elseif (string === "HELLO");
+  {
+    return "YES INDEED!"}
 
   else {
     return "I love you, too."
